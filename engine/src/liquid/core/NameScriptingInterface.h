86d9e9e6d24d694bc392346ef415a7234b9bd85e
gasim <me@gasim.xyz>
2022-07-29 08:49:11 +0200

    Separate scripting interfaces from components
    - Create different scripting interface components
    - Provide separate test files for each scripting interface
    - Create test base class for scripting interface tests

---
+++ engine/src/liquid/core/NameScriptingInterface.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "liquid/scripting/ComponentLuaInterface.h"
+
+namespace liquid {
+
+/**
+ * @brief Scripting interfaces for name component
+ */
+struct NameScriptingInterface {
+  class LuaInterface;
+};
+
+/**
+ * @brief Lua interface for name component
+ */
+class NameScriptingInterface::LuaInterface
+    : public ComponentLuaInterface<NameScriptingInterface::LuaInterface> {
+public:
+  /**
+   * @brief Get name
+   *
+   * @param state Lua state
+   * @return Number of arguments
+   */
+  static int get(void *state);
+
+  /**
+   * @brief Set name
+   *
+   * @param state Lua state
+   * @return Number of arguments
+   */
+  static int set(void *state);
+
+  /**
+   * @brief Interface fields
+   */
+  static constexpr std::array<InterfaceField, 2> fields{
+      InterfaceField{"get", get}, InterfaceField{"set", set}};
+
+  /**
+   * @brief Get component name in scripts
+   *
+   * @return Component name
+   */
+  static const String getName() { return "name"; }
+};
+
+} // namespace liquid
---
+++ engine/tests/liquid-tests/test-utils/ScriptingInterfaceTestBase.h
@@ -0,0 +1,35 @@
+#include "liquid/asset/AssetManager.h"
+#include "liquid/scripting/ScriptingSystem.h"
+
+/**
+ * @brief Test base class for Lua scripting interfaces
+ */
+class LuaScriptingInterfaceTestBase : public ::testing::Test {
+  static const liquid::String ScriptName;
+
+public:
+  /**
+   * @brief Create test base
+   *
+   * @param scriptName Script name
+   */
+  LuaScriptingInterfaceTestBase(const liquid::String &scriptName = ScriptName);
+  /**
+   * @brief Call function
+   *
+   * @param entity Entity
+   * @param functionName Function name
+   * @return Lua scope
+   */
+  liquid::LuaScope &call(liquid::Entity entity,
+                         const liquid::String &functionName);
+
+protected:
+  liquid::EntityDatabase entityDatabase;
+  liquid::EventSystem eventSystem;
+  liquid::AssetManager assetManager;
+  liquid::ScriptingSystem scriptingSystem;
+
+private:
+  liquid::String mScriptName;
+};
